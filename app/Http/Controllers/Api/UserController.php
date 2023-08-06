8f05dd82345eb55a1b02f1d03766d79a1bb00b07
youssefmohamed2000 <yjoe7199@gmail.com>
2023-08-06 15:21:56 +0300

    second-commit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\UserStoreRequest;
 use App\Http\Requests\Api\UserUpdateRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -19,7 +18,9 @@ class UserController extends Controller
 
     public function update(UserUpdateRequest $request, User $user): UserResource
     {
-        $validated = $request->validated();
+        $validated = $request->safe();
+
+        $validated['password'] = Hash::make($validated['password']);
 
         $user->update($validated);
 
